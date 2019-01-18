b6aff7c3ffe1b20b12ebf1f0a0cff7dade43d72b
pr0me7heu2 <brbrasher@gmail.com>
2019-01-18 10:43:12 -0800

    Intro to functions; netcdr intro; and intro to opensci and rnoaa which was super damn cool.

--- fahr_to_celsius.R
+++ fahr_to_celsius.R
@@ -1,10 +1,13 @@
-airtemps <- c(212, 33.5, 90, 100, 0, -40)
+# convert fahrenheit to celsius
 
-celsius1 <- (airtemps[1]-32)*5/9
-celsius2 <- (airtemps[2]-32)*5/9
-celsius3 <- (airtemps[3]-32)*5/9
-
-fahr_to_celsius <- funciton(fahr) {
-    celsius <- (fahr-32)*5/9
-    return(celsius)
+fahr_to_celsius <- function(fahr) {
+  celsius <- (fahr-32)*5/9
+  return(celsius)
 }
+
+# convert celsius to fahrenheit
+celsius_to_fahr <- function(celsius) {
+  fahr <- 9/5*celsius + 32
+  return(fahr)
+}
+
